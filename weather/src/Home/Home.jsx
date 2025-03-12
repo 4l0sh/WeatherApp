dd518ab8089e242ccf40961400687f7304508e7d
ALI <2155399@roc-dev.com>
2025-03-12 14:00:58 +0100

    currentTime

--- weather/src/Home/Home.jsx
+++ weather/src/Home/Home.jsx
@@ -1,20 +1,46 @@
-import React, { Fragment } from 'react';
-import Cloud from '../assets/cloud.png';
+import React, { Fragment, useEffect, useState } from 'react';
+import Cloud from '../assets/cloudy.svg';
 import HourCard from '../components/hourCard';
 import './Home.css';
 
 const Home = () => {
+  const [currTemp, setCurrTemp] = useState(0);
+  useEffect(() => {
+    const clock = document.getElementById('clock');
+    const updateClock = () => {
+      const time = new Date();
+      const hours = time.getHours();
+      const minutes = time.getMinutes();
+      clock.textContent = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+    };
+    updateClock();
+    setInterval(updateClock, 1000);
+
+    fetch(
+      'https://weerlive.nl/api/weerlive_api_v2.php?key=demo&locatie=Amsterdam',
+      {
+        method: 'GET',
+      }
+    ).then((response) => {
+      response.json().then((data) => {
+        setCurrTemp(data.liveweer[0].temp);
+      });
+    });
+  }),
+    [];
   return (
     <Fragment>
       <div className='homeContainer'>
         <div className='topBar'>
           <h1 className='location'>📌 Location</h1>
-          <h1 className='location'>11:30</h1>
+          <h1 className='location' id='clock'>
+            11:30
+          </h1>
         </div>
         <div className='mainContent'>
           <div className='currentWeather'>
             <img className='currentWeatherIcon' src={Cloud} alt='Cloud' />
-            <p className='temp'>7°</p>
+            <p className='temp'>{currTemp}°</p>
           </div>
         </div>
         <div className='Hourly'>
--- weather/src/components/hourCard.jsx
+++ weather/src/components/hourCard.jsx
@@ -1,17 +1,42 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import Cloud from '../assets/cloudy.svg';
+import './hourCard.css';
 
 const HourCard = () => {
+  const hourlyData = [
+    { time: '12:45', temp: '4°', icon: Cloud },
+    { time: '13:45', temp: '9°', icon: Cloud },
+    { time: '14:45', temp: '10°', icon: Cloud },
+    { time: '15:45', temp: '11°', icon: Cloud },
+    { time: '16:45', temp: '12°', icon: Cloud },
+    { time: '17:45', temp: '13°', icon: Cloud },
+    { time: '18:45', temp: '14°', icon: Cloud },
+    { time: '19:45', temp: '15°', icon: Cloud },
+    { time: '20:45', temp: '16°', icon: Cloud },
+  ];
+
+  useEffect(() => {
+    fetch(
+      'https://weerlive.nl/api/weerlive_api_v2.php?key=demo&locatie=Amsterdam',
+      {
+        method: 'GET',
+      }
+    ).then((response) => {
+      response.json().then((data) => {
+        console.log(data.liveweer);
+      });
+    });
+  }, []);
+
   return (
     <Fragment>
-      <div className='hourCard'></div>
-      <div className='hourCard'></div>
-      <div className='hourCard'></div>
-      <div className='hourCard'></div>
-      <div className='hourCard'></div>
-      <div className='hourCard'></div>
-      <div className='hourCard'></div>
-      <div className='hourCard'></div>
-      <div className='hourCard'></div>
+      {hourlyData.map((data, index) => (
+        <div className='hourCard' key={index}>
+          <p>{data.time}</p>
+          <img src={data.icon} alt='Weather Icon' />
+          <h2>{data.temp}</h2>
+        </div>
+      ))}
     </Fragment>
   );
 };
